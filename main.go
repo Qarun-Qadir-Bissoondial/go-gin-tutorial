44f6b64b1dcfa8d89395a084b60c8a4e3a4302c0
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-03-23 20:56:35 -0400

    feat(single-article): gin setup
    set up basic gin router
    serves index page on GET /

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package go_gin_tutorial
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("./templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Home Page",
+		})
+	})
+
+	err := router.Run()
+	if err != nil {
+		log.Fatalln("Could not start application!")
+	}
+}
